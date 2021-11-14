feb440bf8d331fae36fa049527a670394806b7e3
Sasial <44125644+sasial-dev@users.noreply.github.com>
2021-11-14 15:36:09 +1000

    Arrow Functions for intended roblox-ts emit

--- src/expect.d.ts
+++ src/expect.d.ts
@@ -4,43 +4,43 @@ interface assertionMethods {
 	invert: assertions;
 	never: assertions;
 
-	exist(): void;
-	exists(): void;
-	ok(): void;
+	exist: () => void;
+	exists: () => void;
+	ok: () => void;
 
-	equal(expectedValue: unknown): void;
-	equals(expectedValue: unknown): void;
+	equal: (expectedValue: unknown) => void;
+	equals: (expectedValue: unknown) => void;
 
-	isType(expectedType: keyof CheckableTypes): void;
-	aType(expectedType: keyof CheckableTypes): void;
+	isType: (expectedType: keyof CheckableTypes) => void;
+	aType: (expectedType: keyof CheckableTypes) => void;
 
-	isClass(expectedClassName: keyof Instances): void;
-	aClass(expectedClassName: keyof Instances): void;
-	instanceOf(expectedClassName: keyof Instances): void;
+	isClass: (expectedClassName: keyof Instances) => void;
+	aClass: (expectedClassName: keyof Instances) => void;
+	instanceOf: (expectedClassName: keyof Instances) => void;
 
-	error(): void;
-	errors(): void;
-	fail(): void;
-	fails(): void;
-	throw(): void;
-	throws(): void;
+	error: () => void;
+	errors: () => void;
+	fail: () => void;
+	fails: () => void;
+	throw: () => void;
+	throws: () => void;
 
-	match(pattern: string): void;
-	matches(pattern: string): void;
+	match: (pattern: string) => void;
+	matches: (pattern: string) => void;
 
-	contain(expectedValue: unknown): void;
-	contains(expectedValue: unknown): void;
-	has(expectedValue: unknown): void;
+	contain: (expectedValue: unknown) => void;
+	contains: (expectedValue: unknown) => void;
+	has: (expectedValue: unknown) => void;
 
-	containOnly(expectedType: keyof CheckableTypes): void;
-	containsOnly(expectedType: keyof CheckableTypes): void;
-	hasOnly(expectedType: keyof CheckableTypes): void;
+	containOnly: (expectedType: keyof CheckableTypes) => void;
+	containsOnly: (expectedType: keyof CheckableTypes) => void;
+	hasOnly: (expectedType: keyof CheckableTypes) => void;
 
-	near(nearValue: number, nearLimit?: number): void;
-	nears(nearValue: number, nearLimit?: number): void;
-	nearly(nearValue: number, nearLimit?: number): void;
+	near: (nearValue: number, nearLimit?: number) => void;
+	nears: (nearValue: number, nearLimit?: number) => void;
+	nearly: (nearValue: number, nearLimit?: number) => void;
 
-	between(minValue: number, maxValue: number): void;
+	between: (minValue: number, maxValue: number) => void;
 }
 
 declare function expect(value: unknown): assertions;
